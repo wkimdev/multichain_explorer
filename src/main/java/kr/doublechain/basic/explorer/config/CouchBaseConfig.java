51320f30795885ebab50c28ec10ff7cd1dc3ec73
DC-JSK <DC-JSK@DC-JSK>
2018-10-08 16:31:15 +0900

    Task : update query and etc

--- src/main/java/kr/doublechain/basic/explorer/config/CouchBaseConfig.java
+++ src/main/java/kr/doublechain/basic/explorer/config/CouchBaseConfig.java
@@ -16,12 +16,20 @@ public class CouchBaseConfig {
 	@Value("${couchbase.bucket.ip}")
     private String bucketIp;
 
-    public @Bean Cluster cluster() {
+    @Value("${couchbase.bucket.user}")
+    private String userName;
+
+    @Value("${couchbase.bucket.password}")
+    private String bucketPassword;
+	
+    @Bean("couchbaseCluster")
+    public Cluster cluster() {
         CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder()
                 .connectTimeout(30000) 
                 .build();
         System.setProperty("com.couchbase.queryEnabled", "true");
-        return CouchbaseCluster.create(env, bucketIp);
+        CouchbaseCluster couchbaseCluster = CouchbaseCluster.create(env, bucketIp);
+		return couchbaseCluster.authenticate(userName, bucketPassword);
     }
 	
 }
